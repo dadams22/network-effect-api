379e68cf493e008d72320eb7360745e084dc3b4d
David Adams <dadams2212@gmail.com>
2021-05-21 02:05:58 -0700

    Add /profile endpoint to app.py

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ from linkedin_api import Linkedin
 app = Flask(__name__)
 
 
+def create_linkedin_instance():
+    username = request.form['username']
+    password = request.form['password']
+    return Linkedin(username, password)
+
+
+@app.route('/profile', methods=['POST'])
+def get_profile():
+    linkedin = create_linkedin_instance()
+    return linkedin.get_user_profile()
+
+
 if __name__ == '__main__':
     app.run()
